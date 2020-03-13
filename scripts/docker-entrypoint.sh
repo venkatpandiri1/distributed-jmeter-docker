996911cf526addd0ccc962921baf3182e61f6009
venkatpandiri1 <36133925+venkatpandiri1@users.noreply.github.com>
2020-03-13 10:34:57 -0700

    Update docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -8,7 +8,8 @@ case $1 in
     server)
         $JMETER_HOME/bin/jmeter-server \
             -Dserver.rmi.localport=50000 \
-            -Dserver_port=1099
+            -Dserver_port=1099 \
+            -Jserver.rmi.ssl.disable=${SSL_DISABLED}
         ;;
     *)
         echo "Sorry, this option doesn't exist!"
